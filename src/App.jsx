21aba0214c732f2f77cbc2109efd9222bb445353
JavierStark <javiertorralbocortes@gmail.com>
2023-01-18 18:26:38 +0100

    feat: routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import {useState} from 'react'
 import './App.css'
 import Stats from "./components/Stats/Stats.jsx";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import {Route, Routes} from "react-router-dom";
 
-function Header() {
-  return null;
-}
-
-function Footer() {
-  return null;
-}
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path={'/Stats'} element={<Stats/>}/>
+        <Route path={'/'} element={<Stats/>}/>
       </Routes>
       <Footer/>
     </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Call of Cthulhu Character Creator</h1>
+    </div>
+  );
+};
+
+export default Header;
